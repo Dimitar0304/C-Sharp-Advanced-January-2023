ce21fdfe31fbf9cff574e023a1472db7985a7120
Димитър Банчев <114139864+DimitarBanchev@users.noreply.github.com>
2023-01-13 23:27:38 +0200

    Add files via upload

---
+++ StacksAndQueues-Exercises/1. Basic Stack Operations/6. Songs Queue/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _6._Songs_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //on first line we will be given a sequance of songs
+            //on next lines until the queue.count==0 we might be recive command at these type:
+            //Add() add song if it not contains in the queue if it contains print-"{song} is already contained!"
+            //Play() remove a song from queue
+            //Show() - print all queue currently by ,and space
+            string[] songs = Console.ReadLine().Split(", ");
+            Queue<string> songList = new Queue<string>(songs);
+
+            while (songList.Count!=0)
+            {
+                string command = Console.ReadLine();
+                if (command.Length>4)
+                {
+                    string songToAdd = string.Empty;
+                    List<string> commandInfo = command.Split(" ",StringSplitOptions.RemoveEmptyEntries).ToList();
+                    songToAdd = commandInfo[1];
+                    for (int i = 2; i < commandInfo.Count; i++)
+                    {
+                        songToAdd += " " + commandInfo[i];
+                    }
+                    
+                    
+                    
+                    if (!songList.Contains(songToAdd))
+                    {
+                        songList.Enqueue(songToAdd);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{songToAdd} is already contained!");
+                    }
+                }
+                else if (command=="Play")
+                {
+                    if (songList.Count>0)
+                    {
+                        songList.Dequeue();
+                    }
+                    
+                }
+                else if (command=="Show")
+                {
+                    if (songList.Count>0)
+                    {
+
+                        Console.WriteLine($"{string.Join(", ",songList)}");
+                       
+                    }
+                }
+                
+            }
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
---
+++ StacksAndQueues-Exercises/1. Basic Stack Operations/7. Truck Tour/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _7._Truck_Tour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //we have petrol pumps on first line we recive numbers of pumps intiger
+            //with for loop from 0 to <n
+            //we recive pair elements in array [0]index = amount of fuel that current pump give to truck
+            //[1] = is intiger wich is kilometers to next oil pump and 1 liter of fuel == 1 kilometers
+            // find the first oil pump that helps to truck to pass trought all the route of pumps
+
+            int count = int.Parse(Console.ReadLine());
+            Queue<int[]> oilPumps = new Queue<int[]>();
+            for (int i = 0; i < count; i++)
+            {
+                int[] pumps = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                oilPumps.Enqueue(pumps);
+            }
+            int startIndex = 0;
+            while (true)
+            {
+                bool isComplete = true;
+                int totalLiters = 0;
+                foreach (var pump in oilPumps)
+                {
+                    int liters = pump[0];
+                    int distance = pump[1];
+                    totalLiters += liters;
+                    if (totalLiters - distance *1<0)
+                    {
+                        startIndex++;
+                        int[] currentPump = oilPumps.Dequeue();
+                        oilPumps.Enqueue(currentPump); 
+                        isComplete = false;
+                        break;
+                    }
+                    totalLiters -= distance * 1;
+                }
+                if (isComplete==true)
+                {
+                    Console.WriteLine(startIndex);
+                    break;
+                }
+            }
+            
+        }
+    }
+}
