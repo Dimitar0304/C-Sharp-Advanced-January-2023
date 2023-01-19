005d87fae4cb29d26ab5589ea5b3a6a326fa94f8
Димитър Банчев <114139864+DimitarBanchev@users.noreply.github.com>
2023-01-19 19:39:51 +0200

    Add files via upload

---
+++ Exercise-SetAndDictionariesAdvanced/01. Unique Usernames/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Unique_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> names = new HashSet<string>();
+            int countOfInputs = int.Parse(Console.ReadLine());
+            for (int i = 0; i < countOfInputs; i++)
+            {
+                string currentName = Console.ReadLine();
+                names.Add(currentName);
+            }
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
