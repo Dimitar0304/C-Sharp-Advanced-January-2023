f0101abf52d8608940f65239693649dd1d1c7ab4
Димитър Банчев <114139864+DimitarBanchev@users.noreply.github.com>
2023-01-21 18:11:01 +0200

    Add files via upload

---
+++ Streams,File and Directories- Lab/1. Odd Lines/02-LineNumbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace LineNumbers
+{
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            //create a file wich will be input file and read from it 
+            //and create a writer that writes same text with numbers of lines to another output.file
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            // TODO: write your code here…
+            var reader = new StreamReader("../../../input.txt");
+            using (reader)
+            {
+                string line = reader.ReadLine();
+                int lineCounter = 1;
+                using (var writer = new StreamWriter("../../../output.txt"))
+                {
+                    while (line!=null)
+                    {
+                        writer.WriteLine($"{lineCounter} {line}");
+                    }
+                    lineCounter++;
+                }
+            }
+
+        }
+    }
+}
+
+
---
+++ Streams,File and Directories- Lab/1. Odd Lines/Streams, Files and Directories-Lab/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Streams__Files_and_Directories_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //read lines from input.txt to  console with these code
+            var reader = new StreamReader("../../../input.txt");
+            using (reader)
+            {
+                int counter = 1;
+                string line = reader.ReadLine();
+                Console.WriteLine($"{counter} {line}");
+                while (line != null)
+                {
+                    counter++;
+                    if (counter==4)
+                    {
+                        return;
+                    }
+                    line = reader.ReadLine();
+                    Console.WriteLine($"{counter} {line}");
+                    
+                }
+            }
+        }
+    }
+}
