a1a3845a5511b6b6109ca96cff2d6855d9eb7888
Димитър Банчев <114139864+DimitarBanchev@users.noreply.github.com>
2023-01-28 20:09:48 +0200

    Add files via upload

--- FunctionalProgramming-Exercise/05. Applied Arithmetics/Program.cs
+++ FunctionalProgramming-Exercise/05. Applied Arithmetics/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _05._Applied_Arithmetics
 {
@@ -6,7 +8,56 @@ namespace _05._Applied_Arithmetics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+
+            List<int> numbers = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            string command = string.Empty;
+            Action<List<int>> printList = numbers => Console.WriteLine(string.Join(" ",numbers));
+
+            
+
+            Func<List<int>, string, List<int>> commandFunc = (numbers, command) =>
+              {
+                   List<int> result = new List<int>();
+                  switch (command)
+                  {
+                      case"add":
+                          foreach (var number in numbers)
+                          {
+                              result.Add(number + 1);
+                          }
+                          break;
+                         
+                      case "subtract":
+                          foreach (var number in numbers)
+                          {
+                              result.Add(number - 1);
+                          }
+                          break;
+                      case "multiply":
+                          foreach (var number in numbers)
+                          {
+                              result.Add(number * 2);
+                          }
+
+                          break;
+                  }
+                  
+                  return result;
+              };
+
+            while ((command = Console.ReadLine())!="end")
+            {
+                if (command=="print")
+                {
+                    printList(numbers);
+                }
+                else
+                {
+                     numbers = commandFunc(numbers, command);
+                }
+            }
+
         }
     }
 }
