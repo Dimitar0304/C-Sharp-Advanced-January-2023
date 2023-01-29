ad55735ebe418523feaeeacfeae61440f76c80d3
Димитър Банчев <114139864+DimitarBanchev@users.noreply.github.com>
2023-01-29 23:29:48 +0200

    Add files via upload

--- DefiningClasses-Lab/03-CarConstructor/Program.cs
+++ DefiningClasses-Lab/03-CarConstructor/Program.cs
@@ -4,7 +4,7 @@ namespace CarManufacturer
 {
     public class StartUp
     {
-        static void Main(string[] args)
+        static void Main()
         {
             string make = Console.ReadLine();
             string model = Console.ReadLine();
@@ -12,11 +12,13 @@ namespace CarManufacturer
             double fuelQuantity = double.Parse( Console.ReadLine());
             double fuelConsumption = double.Parse(Console.ReadLine());
 
-            Car car1 = new Car();
-            Car car2 = new Car(make, model, year);
+            Car car1 = new Car(make, model, year, fuelQuantity, fuelConsumption);
+            Car car2 = new Car(make, model, year, fuelQuantity, fuelConsumption);
             Car ca3 = new Car(make, model, year, fuelQuantity, fuelConsumption);
 
-            Console.WriteLine();
+            car1.WhoIAm();
+            car2.WhoIAm();
+            ca3.WhoIAm();
 
         }
     }
--- DefiningClasses-Lab/04. Car Engine And Tires/Car.cs
+++ DefiningClasses-Lab/04. Car Engine And Tires/Car.cs
@@ -12,10 +12,16 @@ namespace CarManufacturer
         private int year;
         private double fuelQuantity;
         private double fuelConsumption;
+        private Engine engine;
+        private Tire[] tires;
 
         public Car()
         {
-
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            FuelQuantity = 200;
+            FuelConsumption = 10;
         }
         public Car(string make,string model,int year):this()
         {
@@ -27,11 +33,14 @@ namespace CarManufacturer
         public Car(string make,string model,int year,double fuelQuantity,double fuelConsumption):this()
         {
             
-            this.Make = make;
-            this.Model = model;
-            this.Year = year;
+            
             this. FuelQuantity = fuelQuantity;
             this.FuelConsumption = fuelConsumption;
+        }
+        public Car(string make,string model,int year,double fuelQuantity,double fuelConsumption,Engine engine,Tire[] tires):this(make, model, year, fuelConsumption, fuelQuantity)
+        {
+            Engine = engine;
+            Tires = tires;
         }
         public string Make { get; set; }
         public string Model { get; set; }
@@ -39,8 +48,18 @@ namespace CarManufacturer
         public int Year { get; set; }
         public double FuelQuantity { get; set; }
          public double FuelConsumption { get; set; }
+        public Tire[] Tires
+        {
+            get { return tires; }
+            set { tires = value; }
+        }
 
-       
+        public Engine Engine
+        {
+            get { return engine; }
+            set { engine = value; }
+        }
+
         public void WhoIAm()
         {
             StringBuilder sb = new StringBuilder();
@@ -48,6 +67,11 @@ namespace CarManufacturer
             sb.AppendLine($"Model: {this.Model}");
             sb.AppendLine($"Year: {this.Year}");
             sb.AppendLine($"Fuel: {this.FuelQuantity:F2}");
+            sb.Append($"{this.Engine.Hp} and {this.Engine.CubicCapacity}");
+            foreach (var item in this.Tires)
+            {
+                sb.AppendLine($"{item.Year} - {item.Pressure}");
+            }
             Console.WriteLine(sb);
         }
 
