84fc7e64d7432de010f4ea7642a8bfdaf7ff42ef
Димитър Банчев <114139864+Dimitar0304@users.noreply.github.com>
2023-02-02 23:33:14 +0200

    Add files via upload

---
+++ Difining Classes-Exercise/8. Car Salesman/Car.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _8._Car_Salesman
+{
+    class Car
+    {
+    }
+}
---
+++ Difining Classes-Exercise/8. Car Salesman/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _8._Car_Salesman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
