4c519c1740a6044f718795b9bf0f431bc25dae95
Димитър Банчев <114139864+Dimitar0304@users.noreply.github.com>
2023-02-06 20:45:55 +0200

    Add files via upload

---
+++ Generics-Lab/Demo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Demo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+
+        {
+            List<string> stringArray = new List<string>()
+           {
+               "edno","dwe","tri","4eteri"
+
+           };
+            List<int> itigerList = new List<int>()
+            {
+                1,2,3,4
+            };
+            List<bool> boolList = new List<bool>()
+            {
+                false,false, true,true
+            };
+
+            Print<bool>(boolList);
+
+            void Print<T>(List<T> elements)
+            {
+                int counter = 0;
+                foreach (var item in elements)
+                {
+                    counter++;
+                    Console.Write($"{counter}-{item} ");
+                }
+            }
+
+
+
+
+        }
+       
+    }
+}
